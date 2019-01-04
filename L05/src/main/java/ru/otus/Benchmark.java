f0257b005377d2c9a0df309dac9d7f8f5e179e9b
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-01-04 17:38:18 +0300

    HW#5 Add metrics for GC

--- L05/src/main/java/ru/otus/Benchmark.java
+++ L05/src/main/java/ru/otus/Benchmark.java
@@ -8,11 +8,13 @@ class Benchmark {
     }
 
     void fill(int quantity) throws InterruptedException {
+        System.out.println("Filling benchmark...");
         for (int i = 0; i < payload.length; i++) {
             for (int j = 0; j < quantity; j++) {
                 payload[i] = new byte[j];
             }
-            Thread.sleep(1);
+//            Thread.sleep(1_000);
+            Thread.sleep(100);
         }
     }
 }
--- L05/src/main/java/ru/otus/GcDemo.java
+++ L05/src/main/java/ru/otus/GcDemo.java
@@ -1,48 +1,95 @@
 package ru.otus;
 
 /*
--Xms512m
--Xmx512m
+Program arguments:
+- benchMarkCapacity: 5000
+- benchMarkQuantity 10000
 
-GC's has ben used:
--XX:+UseG1GC
--XX:+UseSerialGC
--XX:+UseParallelGC
+GC param:
+    -Xms512m
+    -Xmx512m
 
-As big data has been used:
-    - capacity 5000
-    - quantity 10000
-Execution time:
-    with 512Mb heap:
-        - G1 36892ms
-        - Serial 32681ms
-        - Parallel 32936ms
-    with 2048Mb heap:
-        - G1 35721ms
-        - Serial 33611ms
-        - Parallel 33352ms
+Results:
+    -XX:+UseParallelGC
+        PS MarkSweep: 0
+        PS Scavenge: 1416
+        GC has been run 157 times per minute
+        Total time: 558451ms
 
-As small data has been used:
-    - capacity 1000
-    - quantity 5000
-Execution time:
-    - G1 3115ms
-    - Serial 3098ms
-    - Parallel 3037ms
+    -XX:+UseG1GC
+        G1 Young Generation: 789
+        G1 Old Generation: 0
+        GC has been run 87 times per minute
+        Total time: 565343ms
+
+    -XX:+UseSerialGC
+        Copy: 1756
+        MarkSweepCompact: 0
+        GC has been run 146 times per minute
+        Total time: 767173ms
 
 Вывод:
-    По скорости выполнения программы лучшими оказались Serial GC и Parallel GC. К тому же их графики (см. screenshots),
-    взятые из VisualVM выглядят стабильней чем у G1. Среднее время работы одной операции у Parallel ниже чем у Serial и
-    G1. Соответственно я делаю вывод, что в моей ситуации Parallel GC оказался лучшим.
+    По скорости выполнения программы лучшими стали ParallelGC и G1GC. Но колличество запусков в минуту у G1 оказалось
+    вдвое меньше чем у Parallel. Исходя из этого можно сделать вывод, что лучшим при данной конфигурации был G1.
  */
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class GcDemo {
     public static void main(String[] args) throws InterruptedException {
-        int capacity = Integer.parseInt(args[0]);
-        int quantity = Integer.parseInt(args[1]);
-        var benchmark = new Benchmark(capacity);
+        int benchMarkCapacity = Integer.parseInt(args[0]);
+        int benchMarkQuantity = Integer.parseInt(args[1]);
+
+        var gcRunCounter = logGcEvents();
+        var benchmark = new Benchmark(benchMarkCapacity);
+
         long beginTime = System.currentTimeMillis();
-        benchmark.fill(quantity);
-        System.out.println("Total time: " + (System.currentTimeMillis() - beginTime) + "ms");
+        benchmark.fill(benchMarkQuantity);
+        long totalTime = System.currentTimeMillis() - beginTime;
+        long totalTimeInMinutes = TimeUnit.MILLISECONDS.toMinutes(totalTime);
+
+        int totalRunQuantity = 0;
+        for(int runQuantity: gcRunCounter.values()) {
+            totalRunQuantity += runQuantity;
+        }
+
+        var gcRunQuantityPerMinute = totalTimeInMinutes != 0 ? totalRunQuantity / totalTimeInMinutes : totalRunQuantity;
+
+        gcRunCounter.forEach((gcName, runQuantity) ->
+            System.out.println(gcName + ": " + runQuantity)
+        );
+
+        System.out.println("GC has been run " + gcRunQuantityPerMinute + " times per minute");
+        System.out.println("Total time: " + totalTime + "ms");
+    }
+
+    private static HashMap<String, Integer> logGcEvents() {
+        HashMap<String, Integer> runCounter = new HashMap<>();
+
+        for (GarbageCollectorMXBean gcMxBean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            runCounter.put(gcMxBean.getName(), 0);
+
+            NotificationListener listener = (notification, handback) -> {
+                var info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                Integer runQuantity = runCounter.get(info.getGcName());
+                runCounter.replace(info.getGcName(), runQuantity, runQuantity + 1);
+            };
+
+            NotificationFilter filter = (notification) ->
+                    notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION);
+
+            NotificationEmitter emitter = (NotificationEmitter) gcMxBean;
+            emitter.addNotificationListener(listener, filter, null);
+        }
+
+        return runCounter;
     }
 }
