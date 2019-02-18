01a60fb537ebcab9c6338d9ed36ae45f01bc3900
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-02-18 20:59:01 +0300

    HW#9 Overload parse object method

--- L09/src/main/java/ru/otus/Serializer.java
+++ L09/src/main/java/ru/otus/Serializer.java
@@ -2,30 +2,62 @@ package ru.otus;
 
 import javax.json.*;
 import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.Collection;
 
 class Serializer {
 	JsonStructure toJson(Object src) {
-		Class<?> clazz = src.getClass();
+//		Class<?> clazz = src.getClass();
+//
+//		if (clazz.isArray()) {
+//			JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
+//			parseObject(jsonArrayBuilder, src);
+//			return jsonArrayBuilder.build();
+//		}
+//
+//		JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+//		parseObject(jsonObjectBuilder, src);
+//		return jsonObjectBuilder.build();
 
-		if (clazz.isArray()) {
-			JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
-			parseObject(jsonArrayBuilder, src);
-			return jsonArrayBuilder.build();
+		JsonArray jsonArray = Json.createArrayBuilder().build();
+		if(src instanceof Collection) {
+			return parseObject(((Collection) src).toArray(), jsonArray);
 		}
 
-		JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
-		parseObject(jsonObjectBuilder, src);
-		return jsonObjectBuilder.build();
+		return parseObject(src, jsonArray);
 	}
 
-	private void parseObject(JsonObjectBuilder jsonObjectBuilder, Object object) {
-		Class<?> nodeClass = object.getClass();
+//	private void parseObject(JsonObjectBuilder jsonObjectBuilder, Object object) {
+//		Class<?> nodeClass = object.getClass();
+//
+//	}
+//
+//	private void parseObject(JsonArrayBuilder jsonArrayBuilder, Object object) {
+//		for (int i = 0; i < Array.getLength(object); i++) {
+//			jsonArrayBuilder.add(i, (JsonValue) Array.get(object, i));
+//		}
+//	}
 
+	JsonStructure parseObject(Object object) {
+		parseObject(object, null);
 	}
 
-	private void parseObject(JsonArrayBuilder jsonArrayBuilder, Object object) {
-		for (int i = 0; i < Array.getLength(object); i++) {
-			jsonArrayBuilder.add(i, (JsonValue) Array.get(object, i));
+	JsonStructure parseObject(Object object, JsonStructure jsonStructure) {
+		Class<?> clazz = object.getClass();
+
+//		if (clazz.isArray()) {
+//			for (int i = 0; i < Array.getLength(object); i++) {
+//
+//			}
+//		}
+
+		Field[] declaredFields = clazz.getDeclaredFields();
+		JsonObjectBuilder objectBuilder = Json.createObjectBuilder(jsonStructure.asJsonObject());
+
+		for (Field field : declaredFields) {
+
 		}
+
+		return objectBuilder.build();
 	}
 }
--- L09/src/main/java/ru/otus/SerializerDemo.java
+++ L09/src/main/java/ru/otus/SerializerDemo.java
@@ -3,6 +3,7 @@ package ru.otus;
 import com.github.javafaker.Faker;
 import com.google.gson.Gson;
 
+import javax.json.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -12,22 +13,39 @@ public class SerializerDemo {
 		Gson gson = new Gson();
 
 		int[] arrayOfInt = {1, 2, 3};
-		System.out.println(serializer.toJson(arrayOfInt));
+		System.out.println(gson.toJson(arrayOfInt));
+//		System.out.println(serializer.toJson(arrayOfInt));
 
-//		Person[] personArray = new Person[10];
-//		Faker faker = new Faker();
-//		for (int i = 0; i < 10; i++) {
-//			personArray[i] = new Person(
-//					faker.funnyName().name(),
-//					faker.number().numberBetween(16, 75),
-//					faker.address().fullAddress(),
-//					faker.bool().bool()
-//			);
-//		}
+		Person[] personArray = new Person[10];
+		Faker faker = new Faker();
+		for (int i = 0; i < 10; i++) {
+			personArray[i] = new Person(
+					faker.funnyName().name(),
+					faker.number().numberBetween(16, 75),
+					faker.address().fullAddress(),
+					faker.bool().bool()
+			);
+		}
+		System.out.println(gson.toJson(personArray));
+
 //		serializer.toJson(personArray);
 //		gson.fromJson("", int[].class);
-//
-//		ArrayList<Person> personArrayList = new ArrayList<>(Arrays.asList(personArray));
+
+		ArrayList<Person> personArrayList = new ArrayList<>(Arrays.asList(personArray));
+		System.out.println(gson.toJson(personArrayList));
 //		serializer.toJson(personArrayList);
+
+		System.out.println("\n\n\n");
+//		JsonObject jsonObject = Json.createObjectBuilder()
+//				.add("firstName", "Duke")
+//				.add("age", 28)
+//				.add("streetAddress", "100 Internet Dr")
+//				.add("phoneNumbers", Json.createArrayBuilder()
+//						.add(Json.createObjectBuilder()
+//								.add("type", "home")
+//								.add("number", "222-222-2222")))
+//				.build();
+		JsonStructure jsonObject = Json.createArrayBuilder().add(1).add("qwerty").build();
+		System.out.println(jsonObject);
 	}
 }
