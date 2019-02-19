565079914ed768d847f6ed9da7ee95b5dcf2f3a8
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-02-19 19:00:12 +0300

    HW#9 Use simple json instead javax json

--- L09/src/main/java/ru/otus/Serializer.java
+++ L09/src/main/java/ru/otus/Serializer.java
@@ -1,12 +1,16 @@
 package ru.otus;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONAware;
+import org.json.simple.JSONObject;
+
 import javax.json.*;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.Collection;
 
 class Serializer {
-	JsonStructure toJson(Object src) {
+	JSONAware toJson(Object src) throws IllegalAccessException {
 //		Class<?> clazz = src.getClass();
 //
 //		if (clazz.isArray()) {
@@ -19,45 +23,124 @@ class Serializer {
 //		parseObject(jsonObjectBuilder, src);
 //		return jsonObjectBuilder.build();
 
-		JsonArray jsonArray = Json.createArrayBuilder().build();
-		if(src instanceof Collection) {
-			return parseObject(((Collection) src).toArray(), jsonArray);
-		}
+//		JsonArray jsonArray = Json.createArrayBuilder().build();
+//		if (src instanceof Collection) {
+//			return parseObject(((Collection) src).toArray(), jsonArray);
+//		}
+//
+//		return parseObject(src, jsonArray);
 
-		return parseObject(src, jsonArray);
+//		JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+//		parseObject(src, arrayBuilder);
+//		return arrayBuilder.build();
+
+		JSONArray jsonArray = new JSONArray();
+		parseObject(src, jsonArray);
+		return jsonArray;
 	}
 
-//	private void parseObject(JsonObjectBuilder jsonObjectBuilder, Object object) {
-//		Class<?> nodeClass = object.getClass();
-//
-//	}
-//
-//	private void parseObject(JsonArrayBuilder jsonArrayBuilder, Object object) {
-//		for (int i = 0; i < Array.getLength(object); i++) {
-//			jsonArrayBuilder.add(i, (JsonValue) Array.get(object, i));
-//		}
-//	}
-
-	JsonStructure parseObject(Object object) {
-		parseObject(object, null);
-	}
-
-	JsonStructure parseObject(Object object, JsonStructure jsonStructure) {
-		Class<?> clazz = object.getClass();
-
-//		if (clazz.isArray()) {
-//			for (int i = 0; i < Array.getLength(object); i++) {
-//
-//			}
-//		}
-
+	private void parseObject(Object o, JSONObject jsonObject) throws IllegalAccessException {
+		Class<?> clazz = o.getClass();
 		Field[] declaredFields = clazz.getDeclaredFields();
-		JsonObjectBuilder objectBuilder = Json.createObjectBuilder(jsonStructure.asJsonObject());
 
 		for (Field field : declaredFields) {
+			field.setAccessible(true);
+			Class<?> fieldType = field.getType();
+			String fieldName = field.getName();
+			Object fieldValue = field.get(o);
 
+			if (fieldType.isArray()) {
+//				JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
+//				builder.add(fieldName, jsonArrayBuilder);
+//				parseObject(fieldValue, jsonArrayBuilder);
+				JSONArray jsonArray = new JSONArray();
+				jsonObject.put(fieldName, jsonArray);
+				parseObject(fieldValue, jsonArray);
+			} else if (fieldType.isPrimitive()) {
+//				if (fieldType == Byte.TYPE) {
+//					builder.add(fieldName, (byte) fieldValue);
+//				}
+//				if (fieldType == Short.TYPE) {
+//					builder.add(fieldName, (short) fieldValue);
+//				}
+//				if (fieldType == Integer.TYPE) {
+//					builder.add(fieldName, (int) fieldValue);
+//				}
+//				if (fieldType == Long.TYPE) {
+//					builder.add(fieldName, (long) fieldValue);
+//				}
+//				if (fieldType == Float.TYPE) {
+//					builder.add(fieldName, (float) fieldValue);
+//				}
+//				if (fieldType == Double.TYPE) {
+//					builder.add(fieldName, (double) fieldValue);
+//				}
+//				if (fieldType == Boolean.TYPE) {
+//					builder.add(fieldName, (boolean) fieldValue);
+//				}
+//				if (fieldType == Character.TYPE) {
+//					builder.add(fieldName, (char) fieldValue);
+//				}
+				jsonObject.put(fieldName, fieldValue);
+			} else if (fieldType.getTypeName().equals("java.lang.String")) {
+//				builder.add(fieldName, (String) fieldValue);
+				jsonObject.put(fieldName, fieldValue);
+			} else {
+//				JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+//				builder.add(fieldName, objectBuilder);
+//				parseObject(fieldValue, objectBuilder);
+				JSONObject jsonObject1 = new JSONObject();
+				jsonObject.put(fieldName, jsonObject1);
+				parseObject(fieldValue, jsonObject1);
+			}
+//			field.setAccessible(false);
 		}
+	}
 
-		return objectBuilder.build();
+	private void parseObject(Object o, JSONArray jsonArray) throws IllegalAccessException {
+		Class<?> clazz = o.getClass();
+		Class<?> componentType = clazz.getComponentType();
+
+		for (int i = 0; i < Array.getLength(o); i++) {
+			Object arrayItem = Array.get(o, i);
+
+			if (componentType.isArray()) {
+//				JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
+//				builder.add(jsonArrayBuilder);
+//				parseObject(arrayItem, jsonArrayBuilder);
+			} else if (componentType.isPrimitive()) {
+				jsonArray.add(arrayItem);
+//				if (componentType == Byte.TYPE) {
+//					builder.add((byte) arrayItem);
+//				}
+//				if (componentType == Short.TYPE) {
+//					builder.add((short) arrayItem);
+//				}
+//				if (componentType == Integer.TYPE) {
+//					builder.add((int) arrayItem);
+//				}
+//				if (componentType == Long.TYPE) {
+//					builder.add((long) arrayItem);
+//				}
+//				if (componentType == Float.TYPE) {
+//					builder.add((float) arrayItem);
+//				}
+//				if (componentType == Double.TYPE) {
+//					builder.add((double) arrayItem);
+//				}
+//				if (componentType == Boolean.TYPE) {
+//					builder.add((boolean) arrayItem);
+//				}
+//				if (componentType == Character.TYPE) {
+//					builder.add((char) arrayItem);
+//				}
+			} else if (componentType.equals("java.lang.String")) {
+				jsonArray.add(arrayItem);
+			} else {
+				JSONObject jsonObject = new JSONObject();
+				jsonArray.add(jsonObject);
+				parseObject(arrayItem, jsonObject);
+			}
+		}
 	}
 }
--- L09/src/main/java/ru/otus/SerializerDemo.java
+++ L09/src/main/java/ru/otus/SerializerDemo.java
@@ -6,46 +6,37 @@ import com.google.gson.Gson;
 import javax.json.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class SerializerDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalAccessException {
 		Serializer serializer = new Serializer();
-		Gson gson = new Gson();
-
+//		Gson gson = new Gson();
+//
 		int[] arrayOfInt = {1, 2, 3};
-		System.out.println(gson.toJson(arrayOfInt));
-//		System.out.println(serializer.toJson(arrayOfInt));
+		boolean[] arrayOfBool = {true, false, true};
+//		System.out.println(gson.toJson(arrayOfInt));
+//
 
-		Person[] personArray = new Person[10];
+
+		Person[] arrayOfPerson = new Person[10];
 		Faker faker = new Faker();
 		for (int i = 0; i < 10; i++) {
-			personArray[i] = new Person(
+			arrayOfPerson[i] = new Person(
 					faker.funnyName().name(),
 					faker.number().numberBetween(16, 75),
 					faker.address().fullAddress(),
 					faker.bool().bool()
 			);
 		}
-		System.out.println(gson.toJson(personArray));
 
-//		serializer.toJson(personArray);
-//		gson.fromJson("", int[].class);
+		ArrayList<Person> personArrayList = new ArrayList<>(Arrays.asList(arrayOfPerson));
 
-		ArrayList<Person> personArrayList = new ArrayList<>(Arrays.asList(personArray));
-		System.out.println(gson.toJson(personArrayList));
-//		serializer.toJson(personArrayList);
+		System.out.println(serializer.toJson(arrayOfInt));
+		System.out.println(serializer.toJson(arrayOfBool));
+		System.out.println(serializer.toJson(arrayOfPerson));
+		System.out.println(serializer.toJson(personArrayList));
 
-		System.out.println("\n\n\n");
-//		JsonObject jsonObject = Json.createObjectBuilder()
-//				.add("firstName", "Duke")
-//				.add("age", 28)
-//				.add("streetAddress", "100 Internet Dr")
-//				.add("phoneNumbers", Json.createArrayBuilder()
-//						.add(Json.createObjectBuilder()
-//								.add("type", "home")
-//								.add("number", "222-222-2222")))
-//				.build();
-		JsonStructure jsonObject = Json.createArrayBuilder().add(1).add("qwerty").build();
-		System.out.println(jsonObject);
+//		System.out.println(Json.createObjectBuilder(stringObjectHashMap).build());
 	}
 }
