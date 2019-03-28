be18400d3a5afee899e9fcbd41f764c9fb327af7
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-03-28 21:35:32 +0200

    HW#12 Create login servlet

--- L12-web-server/src/main/java/ru/otus/service/AuthService.java
+++ L12-web-server/src/main/java/ru/otus/service/AuthService.java
@@ -4,7 +4,7 @@ public class AuthService {
 	private static final String ADMIN_LOGIN = "admin";
 	private static final String ADMIN_PASSWORD = "admin";
 
-	public boolean auth(String login, String password) {
+	public static boolean auth(String login, String password) {
 		return login.equals(ADMIN_LOGIN) && password.equals(ADMIN_PASSWORD);
 	}
 }
