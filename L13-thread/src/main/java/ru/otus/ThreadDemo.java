ff4a5c14d5ce8c5988bcf39bee8b30bd5f2f1670
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-04-15 13:27:02 +0300

    HW#13 Create generator for array

--- L13-thread/src/main/java/ru/otus/ThreadDemo.java
+++ L13-thread/src/main/java/ru/otus/ThreadDemo.java
@@ -1,7 +1,22 @@
 package ru.otus;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class ThreadDemo {
 	public static void main(String[] args) {
-		System.out.println("Thread Demo");
+		int[] array = generateArray(50);
+		System.out.println(Arrays.toString(array));
+	}
+
+	private static int[] generateArray(int size) {
+		int[] ints = new int[size];
+		Random random = new Random();
+
+		for (int i = 0; i < ints.length; i++) {
+			ints[i] = random.nextInt();
+		}
+
+		return ints;
 	}
 }
