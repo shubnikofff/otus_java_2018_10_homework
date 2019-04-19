3b38b979f5528420a493c8485011b233cae511b1
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-04-19 16:05:34 +0300

    HW#13 Change initial array size

--- L13-thread/src/main/java/ru/otus/ThreadDemo.java
+++ L13-thread/src/main/java/ru/otus/ThreadDemo.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ThreadDemo {
 	private static final int THREAD_COUNT = 4;
-	private static final int ARRAY_SIZE = 1_000_000_000;
+	private static final int ARRAY_SIZE = 100_000_000;
 	private static final int ARRAY_ELEMENT_BOUND = 1_000_000;
 	private static final Random random = new Random();
 
