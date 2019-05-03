4c7171ccf4cc9c62cd98e6d1152621769ce8d410
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-05-03 20:28:41 +0300

    HW#14 Create Address

---
+++ L14-message-system/src/main/java/ru/otus/messageSystem/Address.java
@@ -0,0 +1,36 @@
+package ru.otus.messageSystem;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public final class Address {
+	private static final AtomicInteger ID_GENERATOR = new AtomicInteger();
+	private final String id;
+
+	public Address() {
+		id = String.valueOf(ID_GENERATOR.getAndIncrement());
+	}
+
+	public Address(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Address address = (Address) o;
+
+		return Objects.equals(id, address.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+
+	public String getId() {
+		return id;
+	}
+}
