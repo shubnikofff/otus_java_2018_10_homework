e303bb4b5132070286e735c1f72937016669a5f3
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-07-03 23:32:30 +0300

    HW#16 Initial commit

---
+++ L16-multiprocess-application/db-server/src/main/java/ru/otus/DbServer.java
@@ -0,0 +1,7 @@
+package ru.otus;
+
+public class DbServer {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ L16-multiprocess-application/frontend/src/main/java/ru/otus/Frontend.java
@@ -0,0 +1,7 @@
+package ru.otus;
+
+public class Frontend {
+	public static void main(String[] args) {
+
+	}
+}
