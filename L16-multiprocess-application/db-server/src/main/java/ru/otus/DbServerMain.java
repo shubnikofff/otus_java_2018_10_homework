ff12d57768dcb21823214b0e6c58b43ba6d8a091
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-08-01 19:59:24 +0300

    HW#16 Define source encoding for project

--- L16-multiprocess-application/db-server/src/main/java/ru/otus/DbServerMain.java
+++ L16-multiprocess-application/db-server/src/main/java/ru/otus/DbServerMain.java
@@ -1,17 +1,41 @@
 package ru.otus;
 
+import ru.otus.application.service.SocketMessageWorker;
+import ru.otus.message.Message;
+
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DbServerMain {
-	public static void main(String[] args) {
-		try {
-			final Socket socket = new Socket("localhost", 5050);
-			final PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-			writer.println("Hello from " + args[0]);
+	public static void main(String[] args) throws InterruptedException {
+		final Logger logger = Logger.getLogger(args[0]);
+		logger.info("App " + args[0] + " is running on port: " + args[1]);
+
+		try(final ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[1]))) {
+			Socket socket = null;
+			while (socket == null) {
+				socket = serverSocket.accept();
+				logger.log(Level.SEVERE, "Accepted connection on " + socket.getPort());
+			}
+
+			final SocketMessageWorker socketMessageWorker = new SocketMessageWorker(socket);
+			socketMessageWorker.start();
+
+			Thread.sleep(2000);
+			int count = 0;
+			while (true) {
+				final Message message = socketMessageWorker.pollMessage();
+				if(message != null) {
+					System.out.println("Received message from: " + message.getFrom());
+				}
+				Thread.sleep(1000);
+				count++;
+			}
 		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
--- L16-multiprocess-application/frontend/src/main/java/ru/otus/FrontendMain.java
+++ L16-multiprocess-application/frontend/src/main/java/ru/otus/FrontendMain.java
@@ -1,17 +1,37 @@
 package ru.otus;
 
+import ru.otus.application.service.SocketMessageWorker;
+import ru.otus.message.Message;
+
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FrontendMain {
-	public static void main(String[] args) {
-		try {
-			final Socket socket = new Socket("localhost", 5050);
-			final PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-			writer.println("Hello from " + args[0]);
+	public static void main(String[] args) throws InterruptedException {
+		final Logger logger = Logger.getLogger(args[0]);
+		logger.info("App " + args[0] + " is running on port: " + args[1]);
+
+		try(final ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[1]))) {
+			Socket socket = null;
+			while (socket == null) {
+				socket = serverSocket.accept();
+				logger.log(Level.SEVERE, "Accepted connection on " + socket.getPort());
+			}
+
+			final SocketMessageWorker socketMessageWorker = new SocketMessageWorker(socket);
+			socketMessageWorker.start();
+
+			int count = 0;
+			while (count < 10) {
+				socketMessageWorker.sendMessage(new Message(args[0], "DB#1"));
+				Thread.sleep(1000);
+				count++;
+			}
 		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
