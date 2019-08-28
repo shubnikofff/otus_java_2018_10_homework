ea1fff6d32c90bd244bbc648e0bad69fa096a172
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-08-29 01:42:37 +0300

    HW#16 Use servlet component scan

---
+++ L16-multiprocess-application/frontend/src/main/java/ru/otus/FrontendServer.java
@@ -0,0 +1,45 @@
+package ru.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class FrontendServer {
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendServer.class, args);
+	}
+//	public static void main(String[] args) throws InterruptedException {
+//		final Logger logger = Logger.getLogger(args[0]);
+//		logger.info("App " + args[0] + " is running on port: " + args[1]);
+//
+//		try(final ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[1]))) {
+//			Socket socket = null;
+//			while (socket == null) {
+//				socket = serverSocket.accept();
+//				logger.log(Level.SEVERE, "Accepted connection on " + socket.getPort());
+//			}
+//
+//			final MessageWorker socketMessageWorker = new SocketWorker(socket);
+//			socketMessageWorker.start();
+//
+//			int count = 0;
+//			while (count < 50) {
+//				socketMessageWorker.putMessage(new UserListRequest(args[0]));
+//				Thread.sleep(500);
+//				count++;
+//			}
+//
+//			while (true) {
+//				Message message = socketMessageWorker.getMessage();
+//				if(message != null) {
+//					System.out.println("Received message from: " + message.getFrom());
+//				}
+//				Thread.sleep(1000);
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+}
