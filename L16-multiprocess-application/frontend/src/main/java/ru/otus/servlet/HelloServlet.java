c0cc1d3b172d68f306cc701bc02945bf4bacaf90
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-09-02 01:26:10 +0300

    HW#16 Remove frontend service

--- L16-multiprocess-application/frontend/src/main/java/ru/otus/servlet/HelloServlet.java
+++ L16-multiprocess-application/frontend/src/main/java/ru/otus/servlet/HelloServlet.java
@@ -1,29 +1,23 @@
-package ru.otus.servlet;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
-import ru.otus.application.service.FrontendService;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class HelloServlet extends HttpServlet {
-	@Autowired
-	private FrontendService frontendService;
-
-	@Autowired
-	private WebApplicationContext context;
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		req.getServletContext().getAttributeNames();
-		resp.getOutputStream().println("Hello from container " + context.getId());
-		resp.getOutputStream().println("Hello from container " + frontendService.getId());
-	}
-}
+//package ru.otus.servlet;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import ru.otus.application.service.FrontendService;
+//
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.IOException;
+//
+//@Component
+//public class HelloServlet extends HttpServlet {
+//	@Autowired
+//	private FrontendService frontendService;
+//
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//		final HttpSession session = req.getSession();
+//		resp.getOutputStream().println("Hello from container " + System.getenv("ID"));
+//	}
+//}
