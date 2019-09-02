5d44c0222077cb8b4ec1792b2f421b3f16665278
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-09-03 00:05:09 +0300

    HW#16 Inject Dao to the DBService

--- L16-multiprocess-application/db-server/src/main/java/ru/otus/application/configuration/ApplicationConfiguration.java
+++ L16-multiprocess-application/db-server/src/main/java/ru/otus/application/configuration/ApplicationConfiguration.java
@@ -2,13 +2,22 @@ package ru.otus.application.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.otus.application.Application;
 import ru.otus.application.service.DBService;
+import ru.otus.dao.Dao;
+import ru.otus.dao.HibernateDaoFactory;
+import ru.otus.model.Address;
+import ru.otus.model.Phone;
+import ru.otus.model.User;
 
 @Configuration
 public class ApplicationConfiguration {
+	@Value("${hibernate.config.path:hibernate.cfg.xml}")
+	private String hibernateConfigurationPath;
+
 	@Bean(name = "loggerApplication")
 	Logger loggerApplication() {
 		return LoggerFactory.getLogger(Application.class);
@@ -17,5 +26,10 @@ public class ApplicationConfiguration {
 	@Bean(name = "loggerDBService")
 	Logger loggerProcessStarter() {
 		return LoggerFactory.getLogger(DBService.class);
+	}
+
+	@Bean
+	Dao dao() {
+		return new HibernateDaoFactory(hibernateConfigurationPath).getDao(User.class, Address.class, Phone.class);
 	}
 }
