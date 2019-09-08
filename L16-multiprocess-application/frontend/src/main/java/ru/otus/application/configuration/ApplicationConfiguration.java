2d86d77919db3d3caa2b96787b63f19b32ea6e3b
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-09-08 19:46:06 +0300

    HW#16 Remove servlets

--- L16-multiprocess-application/frontend/src/main/java/ru/otus/application/configuration/ApplicationConfiguration.java
+++ L16-multiprocess-application/frontend/src/main/java/ru/otus/application/configuration/ApplicationConfiguration.java
@@ -2,15 +2,27 @@ package ru.otus.application.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import ru.otus.application.Application;
+import ru.otus.web.filter.AuthFilter;
 
 @Configuration
 public class ApplicationConfiguration extends AnnotationConfigWebApplicationContext {
 	@Bean(name = "loggerApplication")
 	Logger loggerApplication() {
 		return LoggerFactory.getLogger(Application.class);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> authFilter() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new AuthFilter());
+		registrationBean.addUrlPatterns("/");
+		registrationBean.addUrlPatterns("/list");
+		registrationBean.addUrlPatterns("/create");
+		return registrationBean;
 	}
 }
